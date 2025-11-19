e5af0c43ab1e64ca0da712d8b83060a08dc8c5ea
Prateek Gupta <kalarimonk@gmail.com>
2025-11-20 00:00:42 +0530

    changed to a N bit priority encoder from 4:2 version

--- src/core_basics/priority_encoder.sv
+++ src/core_basics/priority_encoder.sv
@@ -1,19 +1,33 @@
-module priority_encoder (
-    input logic [3:0] data,
-    output logic [1:0] out
+module priority_encoder #(
+   parameter int INPUT_LINES = 4 ,
+   parameter int OUTPUT_LINES = $clog2(INPUT_LINES) 
+)  (
+    input logic [INPUT_LINES - 1:0] data,
+    output logic [OUTPUT_LINES - 1:0] out,
+    output logic valid
 );
 
-// `casez` uses ?? which means don't care, however It doesn’t treat X (unknown) as a match → safer 
-//  This 4'b1??? matches 4'b1100 or 4'b1010 or even 4'b1Z10 but not 4'b1X10
+initial begin
+    if (INPUT_LINES < 2) begin
+        $error("INPUT_LINES must be >= 2. Got %0d", INPUT_LINES);
+    end
+end
+
 
 always_comb begin
-    casez (data)    
-        4'b1???: out = 2'b11;
-        4'b01??: out = 2'b10;
-        4'b001?: out = 2'b01;
-        4'b0001: out = 2'b00;
-        default: out = 2'b00;
-    endcase
+    valid = 0;
+    out = '0; // default value
+
+    logic flag = 1;
+
+    for (int i = INPUT_LINES - 1; i >= 0 && flag; i--) begin
+        if (data[i]) begin
+            out = i[OUTPUT_LINES - 1:0];
+            valid = 1;
+            flag = 0;
+            // break; //using flag until icarus stops complaining about this
+        end
+    end
 end
      
 endmodule
